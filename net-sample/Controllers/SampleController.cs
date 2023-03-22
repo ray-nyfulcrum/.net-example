0f3450dfaceb3fb0d10a0e11172286f9ef3d900b
Ray Saavedra <rayyacosaavedra@gmail.com>
2023-03-22 21:59:17 +0800

    initial commit

---
+++ net-sample/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace net_sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("helloworld")]
+        public string GetHelloWorld()
+        {
+            return "hello world!";
+        }
+
+        [HttpGet("ping")]
+        public string GetPing()
+        {
+            return "pong!";
+        }
+    }
+}
